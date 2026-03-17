bec0f6147e380c5d015b1c3e512a8fc775795317
Kumkum-Mishra <kumkummishra892004@gmail.com>
2026-03-17 11:25:10 +0530

    Initial commit: added data statistics script

---
+++ data_stats.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+# Load sample dataset
+data = {
+    "Age": [22, 25, 30, 35, 40],
+    "Salary": [20000, 25000, 30000, 35000, 40000]
+}
+
+df = pd.DataFrame(data)
+
+# Print basic statistics
+print("Dataset:\n", df)
+print("\nStatistics:\n", df.describe())
